71a54d4a3d6361103f7e5195942dc218942ca03b
Maksim Kopnov <mko@virtoway.com>
2020-07-22 10:35:04 +0700

    Update index.js

--- vc-module-ci/index.js
+++ vc-module-ci/index.js
@@ -4,6 +4,9 @@ const github = require('@actions/github');
 
 const src = __dirname;
 
+// Required inputs
+const token = core.getInput('token')
+
 // These are option configurations for the @actions/exec lib`
 let output = '';
 let err = '';
@@ -30,6 +33,58 @@ async function installVcGlobalTool() {
     }
 }
 
+//SonarCloud Begin
+async function beginSonarCloud() {
+    console.log('SonarCloud Begin')
+    try {
+        
+        repoName = github.context.repo.repo.substring(github.context.repo.repo.indexOf('/') + 1);
+        branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
+        if (branchName.indexOf('refs/heads/') > -1) {
+            branchName = branchName.slice('refs/heads/'.length);
+        }
+    
+        commandStr = "dotnet sonarscanner begin /k:'VirtoCommerce_" + repoName + "'";
+        commandStr += " /o:'virto-commerce'";
+        commandStr += " /d:sonar.host.url='https://sonarcloud.io'";
+        commandStr += " /d:sonar.login='" + token + "'";
+        commandStr += " /d:sonar.branch='" + branchName + "'";
+
+        await exec.exec(`commandStr`, [], options);
+    } catch (err) {
+        core.setFailed(`Could not Begin SonarCloud because: ${err.message}`);
+        process.exit(1);
+    }
+
+}
+
+//SonarCloud build
+async function buildSonarCloud() {
+    console.log('SonarCloud build')
+    try {
+        await exec.exec(`vc-build`, ['Compile'], options);
+    } catch (err) {
+        core.setFailed(`Could not Build SonarCloud because: ${err.message}`);
+        process.exit(1);
+    }
+
+}
+
+//SonarCloud End
+async function endSonarCloud() {
+    console.log('SonarCloud End')
+    try {
+        commandStr = "dotnet sonarscanner end /d:sonar.login=" + token;
+        await exec.exec(`vc-build`, ['Compile'], options);
+    } catch (err) {
+        core.setFailed(`Could not End SonarCloud because: ${err.message}`);
+        process.exit(1);
+    }
+
+}
+
+
+
 //Build Package
 async function buildPackage() {
     try {
@@ -43,9 +98,11 @@ async function buildPackage() {
 
 
 try {
-    installVcGlobalTool().then(result => { 
-        buildPackage(); 
-    })
+    beginSonarCloud().then(result => { 
+        buildSonarCloud().then(result =>{
+            endSonarCloud()
+        }); 
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
